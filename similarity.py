a16d60ae89eaa16a65827ff197b8dd8c59910602
Nisarg Vadodariya <nisarg72227@gmail.com>
2021-02-20 13:48:31 -0500

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -1,5 +1,4 @@
 import csv
-from math import *
 from flask import Flask, render_template, flash, request
 
 
@@ -23,29 +22,14 @@ def check_similarity(sample_1, sample_2, sample_3):
     similarity_list = []
     for text in range(len(content_list)):
         if text == 0:
-            # similarity_list.append('1')
             similarity = jaccard_similarity(content_list[0], content_list[1])
-            # print(f"The Jaccard similarity between: ",text ," and", text+1,f" equals {similarity:.4f}." "\n")
             similarity_list.append(similarity)
             similarity_text = jaccard_similarity(content_list[0], content_list[2])
-            # print(f"The Jaccard similarity between: ",text ," and", text+2,f" equals {similarity_text:.4f}." "\n")
             similarity_list.append(similarity_text)
         if text == 1:
-            # similarity = jaccard_similarity(content_list[1], content_list[0])
-            # # print(f"The Jaccard similarity between: ",text ," and", text+2,f" equals {similarity:.4f}." "\n")
-            # similarity_list.append(similarity)
-            # similarity_list.append('1')
             similarity_text = jaccard_similarity(content_list[1], content_list[2])
-            # print(f"The Jaccard similarity between: ",text ," and", text-1,f" equals {similarity_text:.4f}." "\n")
             similarity_list.append(similarity_text)
-        # if text == 2:
-        #     similarity = jaccard_similarity(content_list[2], content_list[0])
-        #     # print(f"The Jaccard similarity between: ",text ," and", text-2,f" equals {similarity:.4f}." "\n")
-        #     similarity_list.append(similarity)
-        #     similarity_text = jaccard_similarity(content_list[2], content_list[1])
-        #     # print(f"The Jaccard similarity between: ",text ," and", text-1,f" equals {similarity_text:.4f}." "\n")
-        #     similarity_list.append(similarity_text)
-        #     similarity_list.append('1')
+        
     with open('similarity matrix.csv', 'a') as input:
         writer = csv.writer(input)
         writer.writerow(similarity_list)
@@ -84,46 +68,5 @@ def similarity():
     return render_template('result.html', First=one_comp, Second=two_comp, Third=three_comp)
 
 
-# @app.route('/result/', methods=["POST", "GET"])
-# def result():
-#     return render_template('textSimilarity.html')
-
-
 if __name__ == "__main__":
     app.run()
-
-
-#
-#
-# if __name__ == '__main__':
-#     sample_1 = "The easiest way to earn points with Fetch Rewards is to just shop for the products you already love. If you have any participating brands on your receipt, you'll get points based on the cost of the products. You don't need to clip any coupons or scan individual barcodes. Just scan each grocery receipt after you shop and we'll find the savings for you."
-#     sample_2 = "The easiest way to earn points with Fetch Rewards is to just shop for the items you already buy. If you have any eligible brands on your receipt, you will get points based on the total cost of the products. You do not need to cut out any coupons or scan individual UPCs. Just scan your receipt after you check out and we will find the savings for you."
-#     sample_3 = "We are always looking for opportunities for you to earn more points, which is why we also give you a selection of Special Offers. These Special Offers are opportunities to earn bonus points on top of the regular points you earn every time you purchase a participating brand. No need to pre-select these offers, we'll give you the points whether or not you knew about the offer. We just think it is easier that way."
-    # webcrwal(sample_1, sample_2, sample_3)
-
-
-    # with open('similarity matrix.csv', 'r') as readfile:
-    #     list = [line.split('') for line in readfile]
-    #     # for i in range(len(list[0])):
-        #     for j in range(len(list[0])):
-        #         cosine(list[i], list[j])
-
-
-    #
-    # word_count = pd.read_csv("F:/count.csv")
-    # words = word_count.drop(columns=['data','mining'])
-    # print(words)
-    # print(words.columns)
-    #
-    #
-    # # Normalize the data attributes for the Iris dataset.
-    # normalized_X = preprocessing.normalize(words)
-    #
-    # print(normalized_X)
-    # print(normalized_X[1,:])
-    #
-    # for i in range(5):
-    #     for j in range(5):
-    #         num = normalized_X[i, :]
-    #         den = normalized_X[j, :]
-    #         cosine(num,den)
